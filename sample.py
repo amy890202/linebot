ab44be7f86d25e00929a3e4d01b6193603c9ff74
amy890202 <52898990+amy890202@users.noreply.github.com>
2019-07-15 11:29:21 +0800

    Update sample.py

--- sample.py
+++ sample.py
@@ -43,9 +43,8 @@ def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
         message=ImageSendMessage(
-            original_content_url='https://pokemon.gameinfo.io/images/pokemon-go/143-00.png',
-            preview_image_url='https://pokemon.gameinfo.io/images/pokemon-go/143-00.png'
-        )
+            original_content_url='https://imgur.com/QbPVD6w',
+            preview_image_url='https://imgur.com/QbPVD6w'
         line_bot_api.reply_message(event.reply_toke, message)
 
 
